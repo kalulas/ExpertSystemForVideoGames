d236cf44505bbb5efff108374bb0b375ef528516
KaLuLas <499485532@qq.com>
2019-11-30 20:37:59 +0800

    用户检索，内容显示功能完成

--- ExpertSystem/VideoGame.py
+++ ExpertSystem/VideoGame.py
@@ -1,5 +1,5 @@
 import math
-
+NORECORD = 'NORECORD'
 
 class VideoGame:
     games = []
@@ -9,27 +9,35 @@ class VideoGame:
     
     def __init__(self, data):
         # 初始化对象
+        # 数据缺失替换为字符串no_record
         self.name = data.Name
         self.platform = data.Platform
-
-        if not math.isnan(float(data.Year_of_Release)):
+        # if not math.isnan(float(data.Year_of_Release)):
+        try:
             self.year_of_release = int(data.Year_of_Release) 
-        else:
-            self.year_of_release = -1
+        except ValueError:
+            self.year_of_release = NORECORD
         
         self.genre = data.Genre
         self.publisher = data.Publisher
         self.global_sales = data.Global_Sales
-        self.critic_score = data.Critic_Score
-        self.user_score = data.User_Score
+
+        try:
+            self.critic_score = int(data.Critic_Score)
+        except ValueError:
+            self.critic_score = NORECORD
+        try:
+            self.user_score = round(float(data.User_Score), 1)
+        except ValueError:
+            self.user_score = NORECORD
         self.developer = data.Developer
         self.rating = data.Rating
-        # 记录类型
+        # 记录游戏类型
         VideoGame.Platform.add(self.platform)
-        # 避免出现NaN游戏类型
-        if type(self.genre) != float:
+        # 避免在下拉菜单中出现NaN游戏类型
+        if self.genre != NORECORD:
             VideoGame.Genre.add(self.genre)
-        if self.year_of_release != -1:
+        if self.year_of_release != NORECORD:
             VideoGame.YearOfRelease.add(int(self.year_of_release))
         VideoGame.games.append(self)
 
--- ExpertSystem/expert.py
+++ ExpertSystem/expert.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import pandas as pd
 import tkinter as tk
 import tkinter.font as tkFont
@@ -6,17 +7,81 @@ from tkinter import ttk, scrolledtext
 # from tkinter import scrolledtext
 from VideoGame import VideoGame
 
+NORECORD = 'NORECORD'
 # 关联九种不同rating的intVar列表，为1时表示被选中
 intVar = []
-# game_list = []
+# 当前显示第n条记录，通过按钮进行切换
+selection = 0
+game_list = []
+# 满足用户搜索条件的游戏
+properties = []
 # printMaximum = 1
-rating_list =  ['nan', 'E10+', 'T', 'K-A', 'RP', 'E', 'EC', 'AO', 'M']
+rating_list =  ['E10+', 'T', 'K-A', 'RP', 'E', 'EC', 'AO', 'M']
 # os.chdir('../video-game-sales-with-ratings/')
 csv_filepath = './video-game-sales-with-ratings/Video_Games_Sales.csv'
+
+
+def change_display():
+    global properties, selection
+    display_message = '\n为您找到 {} 款游戏，当前显示第 {} 项\n\n游戏名称：{}\n游戏类型：{}\n发售时间：{}\n发售平台：{}\n开发者：{}\n媒体评分：{}\n大众评分：{}\n游戏销量（百万）：{}\n游戏评级：{}\n'.format(
+                            len(properties), selection+1,
+                            properties[selection].name, properties[selection].genre,properties[selection].year_of_release, properties[selection].platform,
+                            properties[selection].developer, properties[selection].critic_score,properties[selection].user_score, properties[selection].global_sales,
+                            properties[selection].rating)
+    return display_message
+
+
+def properties_filter():
+    global properties, selection
+    properties.clear()
+    # 得到界面中用户选择的查询条件 
+    platform = platform_select.get()
+    genre = genre_select.get()
+    l_bound = int(from_year_select.get())
+    r_bound = int(to_year_select.get())
+    critical_score_l = int(critical_score_scale.get())
+    user_score_l = round((user_score_scale.get()),1)
+    # 多项可选评级审查
+    allowed_rating = []
+    for idx in range(len(intVar)):
+        if intVar[idx].get():
+            allowed_rating.append(rating_list[idx])
+    print('RULE: ',platform, genre, l_bound, r_bound, critical_score_l, user_score_l)
+    for game in game_list:
+        if game.platform == platform and game.genre == genre \
+            and (game.year_of_release == NORECORD or game.year_of_release >= l_bound and game.year_of_release <= r_bound)\
+                and (game.critic_score == NORECORD or game.critic_score >= critical_score_l) \
+                    and(game.user_score == NORECORD or game.user_score >= user_score_l)\
+                        and(game.rating == NORECORD or game.rating in allowed_rating):
+            properties.append(game)
+    print('RESULT: ', len(properties))
+    # 在窗口中显示符合用户要求的首条记录
+    # 检测结果条数是否 > 0
+    if len(properties):
+        selection = 0
+        result_message['text'] = change_display()
+    else:
+        result_message['text'] = '数据库中没有符合要求的游戏'
+
+        
+def next_message():
+    global properties, selection
+    if selection < len(properties)-1:
+        selection += 1
+        result_message['text'] = change_display()
+
+
+def prev_message():
+    global properties, selection
+    if selection > 0:
+        selection -= 1
+        result_message['text'] = change_display()
 
 if __name__ == '__main__':
     # TODO: 初始化，考虑封装为函数
     dataFrame = pd.read_csv(csv_filepath)
+    # 在这里对nan数据进行处理
+    dataFrame.fillna(NORECORD,inplace=True)
     for index, row in dataFrame.iterrows():
         VideoGame(row)
     game_list = VideoGame.games
@@ -32,8 +97,8 @@ if __name__ == '__main__':
     window.resizable(width=False, height=False)
 
     # 第0行与第一行放置给用户的推荐游戏信息
-    message = tk.Label(window, text='[EXPERT SYSTEM]根据您给出的要求，为您推荐以下游戏', font=('Microsoft YaHei', 18))
-    result_window = tk.Frame(window, width=1024, height=250)
+    message = tk.Label(window, text='[EXPERT SYSTEM]', font=('Microsoft YaHei', 18))
+    result_window = tk.Frame(window, width=1024, height=200)
     # 保证窗口大小不变
     result_window.propagate(0)
     message.grid(row=0, columnspan=5)
@@ -42,17 +107,17 @@ if __name__ == '__main__':
     result_window.grid(row=1, columnspan=5)
 
     # 第二行设置按钮，有多条推荐信息时用按钮进行切换
-    prev_btn = tk.Button(window, text='上一条')
-    next_btn = tk.Button(window, text='下一条')
+    prev_btn = tk.Button(window, text='上一条', command=prev_message)
+    next_btn = tk.Button(window, text='下一条', command=next_message)
     prev_btn.grid(row=2, column=3, sticky='e', ipadx=20, pady=30)
     next_btn.grid(row=2, column=4, ipadx=20)
 
     # 第三行用于选择游戏所在平台以及游戏类型
     platform_label = tk.Label(window, text='主机平台', font=('tMicrosoft YaHei',12,'bold'))
     genre_label = tk.Label(window, text='游戏类型', font=('tMicrosoft YaHei',12,'bold'))
-    platform_select = ttk.Combobox(window, value=list(VideoGame.Platform))
+    platform_select = ttk.Combobox(window, value=sorted(list(VideoGame.Platform)))
     platform_select.current(0)
-    genre_select = ttk.Combobox(window, value=list(VideoGame.Genre))
+    genre_select = ttk.Combobox(window, value=sorted(list(VideoGame.Genre)))
     genre_select.current(0)
     platform_label.grid(row=3, column=0)
     platform_select.grid(row=3, column=1)
@@ -66,7 +131,7 @@ if __name__ == '__main__':
     from_year_select = ttk.Combobox(window, value=list(VideoGame.YearOfRelease))
     from_year_select.current(0)
     to_year_select = ttk.Combobox(window, value=list(VideoGame.YearOfRelease))
-    to_year_select.current(0)
+    to_year_select.current(len(VideoGame.YearOfRelease)-1)
     time_range_labelA.grid(row=4, column=0)
     time_range_labelB.grid(row=4, column=2)
     from_year_select.grid(row=4, column=1)
@@ -81,7 +146,7 @@ if __name__ == '__main__':
     user_score_scale.grid(row=5, column=2, columnspan=2)
 
     # 第六行，确认提交所选游戏指标要求
-    submit_btn = tk.Button(window, text='提交', font=('Microsoft YaHei', 15))
+    submit_btn = tk.Button(window, text='提交', font=('Microsoft YaHei', 15), command=properties_filter)
     submit_btn.grid(row=6, column=2, ipadx=70, ipady=10, pady=10)
 
     # 最右列放置一个用于选择游戏分级的listGroup
@@ -89,9 +154,9 @@ if __name__ == '__main__':
     rating_frame.grid(row=3, column=4, rowspan=3)
     rating_note_label = tk.Label(rating_frame, text='游戏评级选择', font=('tMicrosoft YaHei',12,'bold'))
     rating_note_label.pack()
-    for i in range(9):
-        intVar.append(tk.IntVar)
-        check = tk.Checkbutton(rating_frame, text=rating_list[i], variable=intVar[i], onvalue=1, offvalue=0)
+    for idx in range(len(rating_list)):
+        intVar.append(tk.IntVar(value=1))
+        check = tk.Checkbutton(rating_frame, text=rating_list[idx], variable=intVar[idx], onvalue=1, offvalue=0)
         check.pack(side='top', expand='yes', fill='both')
 
     window.mainloop()
